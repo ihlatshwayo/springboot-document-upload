6611106a9c6ec9234e2ab75e0495cfd5b77ef908
f3663787 <ihlatshwayo@fnb.co.za>
2020-08-14 19:37:00 +0200

    update post functionality

--- src/main/java/qa/qbd/springbootdocumentupload/controller/PostController.java
+++ src/main/java/qa/qbd/springbootdocumentupload/controller/PostController.java
@@ -4,23 +4,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import qa.qbd.springbootdocumentupload.dao.DocumentRepository;
+import qa.qbd.springbootdocumentupload.entity.Document;
 import qa.qbd.springbootdocumentupload.entity.Post;
 import qa.qbd.springbootdocumentupload.feignclient.PostServiceProxy;
 import qa.qbd.springbootdocumentupload.service.DocumentService;
+
+import java.util.Optional;
 
 @RestController
 public class PostController {
 
     @Autowired
-    private DocumentService documentService;
+    private DocumentRepository documentRepository;
 
     @Autowired
     PostServiceProxy postServiceProxy;
 
-    @RequestMapping("/posts/{id}")
-    public void addPostToDocument(@PathVariable long id){
+    @RequestMapping("/posts/{id}/{userId}/{docId}")
+    public void addPostToDocument(@PathVariable long id,@PathVariable int userId,@PathVariable long docId){
         Post post = postServiceProxy.findPostById(id);
         System.out.println("post title: "+post.getTitle());
+        post.setUserId(userId);
+        Optional<Document> userDocument = documentRepository.findDocumentByIdAndUserId(docId,userId);
+        userDocument.get().setPost(post);
+        documentRepository.save(userDocument.get());
 
     }
 }
