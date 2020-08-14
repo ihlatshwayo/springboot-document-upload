9a8dde18d9b665771bfd18754b143178cd04255e
f3663787 <ihlatshwayo@fnb.co.za>
2020-08-14 19:37:12 +0200

    update post functionality

--- src/main/java/qa/qbd/springbootdocumentupload/dao/DocumentRepository.java
+++ src/main/java/qa/qbd/springbootdocumentupload/dao/DocumentRepository.java
@@ -5,9 +5,11 @@ import qa.qbd.springbootdocumentupload.entity.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DocumentRepository extends JpaRepository< Document, Long> {
 
     List<Document> findByUserId(int userId);
+    Optional<Document>  findDocumentByIdAndUserId(long docId, int userId);
 }
