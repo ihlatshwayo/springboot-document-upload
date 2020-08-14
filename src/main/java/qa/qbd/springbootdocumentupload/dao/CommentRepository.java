30070f5c92f7c0d973d8998b85dd67fbb1f4f4a4
f3663787 <ihlatshwayo@fnb.co.za>
2020-08-14 22:27:21 +0200

    updated comment reosirtory with retrieve all comments for user

--- src/main/java/qa/qbd/springbootdocumentupload/dao/CommentRepository.java
+++ src/main/java/qa/qbd/springbootdocumentupload/dao/CommentRepository.java
@@ -4,9 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import qa.qbd.springbootdocumentupload.entity.*;
 
+import java.util.List;
+
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    public Comment findCommentById(int userId);
+    public Comment findCommentByUserIdAndPostId(int userId, int postId);
+
+    public List<Comment> findCommentByUserId(int userId);
 }
