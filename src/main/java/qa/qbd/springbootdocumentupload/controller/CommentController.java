ebdfcf9e9c79945462617c035c5a1abd172d7ef5
f3663787 <ihlatshwayo@fnb.co.za>
2020-08-16 02:59:31 +0200

    revoiew update

--- src/main/java/qa/qbd/springbootdocumentupload/controller/CommentController.java
+++ src/main/java/qa/qbd/springbootdocumentupload/controller/CommentController.java
@@ -1,10 +1,28 @@
 package qa.qbd.springbootdocumentupload.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import qa.qbd.springbootdocumentupload.dao.PostRepository;
+import qa.qbd.springbootdocumentupload.entity.Comment;
+import qa.qbd.springbootdocumentupload.feignclient.PostServiceProxy;
+
+import java.util.List;
+
+@RestController
 public class CommentController {
 
-    /*@RequestMapping("/posts/{id}/comments")
-    public void addCommentToDocument(@PathVariable long id){
-        Post post = postServiceProxy.findPostById(id);
+    @Autowired
+    PostServiceProxy postServiceProxy;
 
-    }*/
+    @Autowired
+    PostRepository postRepository;
+
+    @GetMapping("/posts/{postId}/comments/{userId}")
+    public void getCommentAndAddToDocumentPost(@PathVariable long postId, @PathVariable int userId){
+
+
+
+    }
 }
