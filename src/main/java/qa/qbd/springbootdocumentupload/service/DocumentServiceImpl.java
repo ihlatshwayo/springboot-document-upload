fe977da39ce248e7c98f1edb1cb44d3a9b2123ea
f3663787 <ihlatshwayo@fnb.co.za>
2020-08-16 02:58:50 +0200

    revoiew update

--- src/main/java/qa/qbd/springbootdocumentupload/service/DocumentServiceImpl.java
+++ src/main/java/qa/qbd/springbootdocumentupload/service/DocumentServiceImpl.java
@@ -2,26 +2,31 @@ package qa.qbd.springbootdocumentupload.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.multipart.MultipartFile;
 import qa.qbd.springbootdocumentupload.dao.CommentRepository;
 import qa.qbd.springbootdocumentupload.dao.DocumentRepository;
 import qa.qbd.springbootdocumentupload.dao.PostRepository;
 import qa.qbd.springbootdocumentupload.entity.*;
+import qa.qbd.springbootdocumentupload.feignclient.PostServiceProxy;
+
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class DocumentServiceImpl  implements DocumentService{
+public class DocumentServiceImpl  implements DocumentService {
 
-    @Autowired
-    private DocumentRepository documentRepository;
 
     @Autowired
     private PostRepository postRepository;
 
     @Autowired
     private CommentRepository commentRepository;
+
+    @Autowired
+    private DocumentRepository documentRepository;
+
 
     @Override
     public Object addDocument(MultipartFile multipartFile, int userId) throws IOException {
@@ -32,7 +37,8 @@ public class DocumentServiceImpl  implements DocumentService{
         document.setFileName(multipartFile.getOriginalFilename());
         document.setUserId(userId);
 
-;       documentRepository.save(document);
+        ;
+        documentRepository.save(document);
 
         return null;
     }
@@ -49,41 +55,6 @@ public class DocumentServiceImpl  implements DocumentService{
         return "Successfully deleted the document";
     }
 
-    @Override
-    public String addPost(Post post, int userId) {
-        //design user document retrival
-        //Optional<Document> document =  documentRepository.findByUserId(userId);
-
-       // document.get().setPost(post);
-        postRepository.save(post);
-        return "post succesfully added to document";
-    }
-
-    @Override
-    public String addComment(Comment comment, int userId) {
-        commentRepository.save(comment);
-        return "Comment succesfully added to document";
-    }
-
-    public  void createDocumentPost(){
-
-        // response
-
-      Optional<Document> document =  documentRepository.findById(1L) ;
-       // post api and set post
-
-        Post post = new Post();
-
-        //post.setBody();
-
-        document.get().setPost(post);
-
-        //documentRepository.save(document);
-
-
-    }
-    public void createDocumentComment(){
-
-
-    }
 }
+
+
