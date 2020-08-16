f18aad4553b01688342480b64d1fe902f0fa83ed
f3663787 <ihlatshwayo@fnb.co.za>
2020-08-16 02:59:17 +0200

    revoiew update

--- src/main/java/qa/qbd/springbootdocumentupload/controller/PostController.java
+++ src/main/java/qa/qbd/springbootdocumentupload/controller/PostController.java
@@ -3,34 +3,18 @@ package qa.qbd.springbootdocumentupload.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import qa.qbd.springbootdocumentupload.dao.DocumentRepository;
-import qa.qbd.springbootdocumentupload.entity.Document;
-import qa.qbd.springbootdocumentupload.entity.Post;
-import qa.qbd.springbootdocumentupload.feignclient.PostServiceProxy;
-import qa.qbd.springbootdocumentupload.service.DocumentService;
-
-import java.util.Optional;
+import qa.qbd.springbootdocumentupload.service.PostService;
 
 @RestController
 public class PostController {
 
     @Autowired
-    private DocumentRepository documentRepository;
+    PostService postService;
 
-    @Autowired
-    PostServiceProxy postServiceProxy;
-
-    @GetMapping("/posts/{id}/{userId}/{docId}")
-    public void getPostAndAddToDocument(@PathVariable long id,@PathVariable int userId,@PathVariable long docId){
-        Post post = postServiceProxy.findPostById(id);
-        post.setUserId(userId);
-        Optional<Document> userDocument = documentRepository.findDocumentByIdAndUserId(docId,userId);
-
-        userDocument.ifPresent(document -> document.setPost(post));
-
-        documentRepository.save(userDocument.get());
-
+    @GetMapping("/posts/{postId}/{userId}/{docId}")
+    public void getPostAndAddToDocument(@PathVariable long postId,@PathVariable int userId,@PathVariable long docId){
+        postService.getPostAndAddToDocument(postId,userId,docId);
     }
+
 }
