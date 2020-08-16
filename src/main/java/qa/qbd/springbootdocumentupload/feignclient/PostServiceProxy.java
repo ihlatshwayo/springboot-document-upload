9cd5ab593941db60840312e5389dbfa8b26079a1
f3663787 <ihlatshwayo@fnb.co.za>
2020-08-16 02:59:04 +0200

    revoiew update

--- src/main/java/qa/qbd/springbootdocumentupload/feignclient/PostServiceProxy.java
+++ src/main/java/qa/qbd/springbootdocumentupload/feignclient/PostServiceProxy.java
@@ -4,7 +4,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import qa.qbd.springbootdocumentupload.entity.Comment;
 import qa.qbd.springbootdocumentupload.entity.Post;
+
+import java.util.List;
 
 
 @FeignClient(name="jsonplaceholder", url = "https://jsonplaceholder.typicode.com")
@@ -12,11 +15,12 @@ public interface PostServiceProxy {
     @GetMapping("/posts/{postId}")
     public Post findPostById(@PathVariable Long postId);
 
-    @GetMapping("/posts/{postId}/comments")
-    public void getCommentAndAddToDocument(@PathVariable long postId);
 
     @GetMapping("/posts/{postId}/{userId}/{docId}")
     public void getPostAndAddToDocument(@PathVariable long postId,@PathVariable int userId,@PathVariable long docId);
+
+    @GetMapping("/posts/{postId}/comments")
+    public List<Comment> getCommentAndAddToDocumentPost(@PathVariable long postId);
 
 
 }
