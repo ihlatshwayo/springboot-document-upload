08ed3d8270d25c8679bd45e5f00e256cfae00801
f3663787 <ihlatshwayo@fnb.co.za>
2020-08-17 08:21:50 +0200

    update commit

--- src/test/java/qa/qbd/springbootdocumentupload/SpringbootDocumentUploadApplicationTests.java
+++ src/test/java/qa/qbd/springbootdocumentupload/SpringbootDocumentUploadApplicationTests.java
@@ -2,12 +2,13 @@ package qa.qbd.springbootdocumentupload;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import qa.qbd.springbootdocumentupload.dto.PostDto;
+import qa.qbd.springbootdocumentupload.entity.Post;
 
 @SpringBootTest
 class SpringbootDocumentUploadApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
+
 
 }
