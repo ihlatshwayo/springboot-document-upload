4fc375cbf94b5664c1339a6c27cfa1c7029ba539
f3663787 <ihlatshwayo@fnb.co.za>
2020-08-17 08:22:22 +0200

    update commit

--- src/main/java/qa/qbd/springbootdocumentupload/SpringbootDocumentUploadApplication.java
+++ src/main/java/qa/qbd/springbootdocumentupload/SpringbootDocumentUploadApplication.java
@@ -3,9 +3,11 @@ package qa.qbd.springbootdocumentupload;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableRetry
 public class SpringbootDocumentUploadApplication {
 
     public static void main(String[] args) {
