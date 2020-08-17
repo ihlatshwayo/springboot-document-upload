f05bf45047a89244b61e90c744fc7b1a3931e840
f3663787 <ihlatshwayo@fnb.co.za>
2020-08-17 08:15:17 +0200

    update commit

---
+++ src/main/java/qa/qbd/springbootdocumentupload/dto/ErrorDTO.java
@@ -0,0 +1,13 @@
+package qa.qbd.springbootdocumentupload.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ErrorDTO {
+
+    private int errorCode;
+    private String errorMessage;
+
+}
