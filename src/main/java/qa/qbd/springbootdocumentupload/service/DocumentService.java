b7b7f698e388c122f86261619585b7c81932db09
f3663787 <ihlatshwayo@fnb.co.za>
2020-08-17 08:16:04 +0200

    update commit

--- src/main/java/qa/qbd/springbootdocumentupload/service/DocumentService.java
+++ src/main/java/qa/qbd/springbootdocumentupload/service/DocumentService.java
@@ -1,10 +1,7 @@
 package qa.qbd.springbootdocumentupload.service;
 
-import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import qa.qbd.springbootdocumentupload.entity.Comment;
 import qa.qbd.springbootdocumentupload.entity.Document;
-import qa.qbd.springbootdocumentupload.entity.Post;
 
 import java.io.IOException;
 import java.util.List;
@@ -12,11 +9,13 @@ import java.util.List;
 
 public interface DocumentService {
 
-    Object addDocument(MultipartFile multipartFile, int userId) throws IOException;
+    Object addDocument(MultipartFile multipartFile, long userId) throws IOException, Exception;
 
-    List<Document> getUserDocuments(int userId);
+    List<Document> getUserDocuments(long userId) throws Exception;
 
-    String deleteDocument(long documentId);
+    String deleteDocument(long documentId) throws Exception;
+
+
 
 
 
